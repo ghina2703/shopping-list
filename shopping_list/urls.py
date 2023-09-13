ce54ccb29e568e5a8e5495525db83350fdff1f95
Ghina Nabila Gunawan <124442361+ghina2703@users.noreply.github.com>
2023-09-13 15:01:55 +0700

    Tutorial 2 Selesai

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -19,10 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
 ]
-
-# Berkas urls.py pada proyek bertanggung jawab untuk mengatur rute URL tingkat proyek.
-# Fungsi include digunakan untuk mengimpor rute URL dari aplikasi lain (dalam hal ini, dari aplikasi main) ke dalam berkas urls.py proyek.
-# Path URL 'main/' akan diarahkan ke rute yang didefinisikan dalam berkas urls.py aplikasi main.
-# atau bisa juga menggunakan path URL ''
