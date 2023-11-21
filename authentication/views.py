f7cad6bedfda8443e1a2413887a676d97d6b8a68
ghina2703 <ghina2703@gmail.com>
2023-11-22 00:26:13 +0700

    deploy ulang tutorial 8

--- authentication/views.py
+++ authentication/views.py
@@ -2,78 +2,46 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+import json
 
+@csrf_exempt
+def register(request):
+    try:
+        data = json.loads(request.body)
+        username = data.get('username', '')
+        password = data.get('password', '')
 
-# @csrf_exempt
-# def login(request):
-#     if request.method == 'POST':
-#         # Check if 'username' and 'password' keys are present in request.POST
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
+        # Check if username and password are provided
+        if not username or not password:
+            return JsonResponse({
+                'status': False,
+                'message': 'Username and password are required fields.'
+            }, status=400)
 
-#         if username is not None and password is not None:
-#             user = authenticate(username=username, password=password)
+        # Check if the username is already taken
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({
+                'status': False,
+                'message': 'Username is already taken. Choose a different one.'
+            }, status=400)
 
-#             if user is not None:
-#                 if user.is_active:
-#                     auth_login(request, user)
-#                     # Status login sukses.
-#                     return JsonResponse({'status': 'success'})
-#                 else:
-#                     return JsonResponse({'status': 'inactive'})
-#             else:
-#                 return JsonResponse({'status': 'invalid_credentials'})
-#         else:
-#             return JsonResponse({'status': 'missing_fields'})
-#     else:
-#         # Handle cases where the request method is not POST
-#         return JsonResponse({'status': 'method_not_allowed'})
+        # Create a new user
+        user = User.objects.create_user(username=username, password=password)
 
-# @csrf_exempt
-# def logout(request):
-#     username = request.user.username
+        return JsonResponse({
+            'status': True,
+            'message': 'User registered successfully.',
+            'username': user.username
+        }, status=201)
+    
+    except Exception as e:
+        return JsonResponse({
+            'status': False,
+            'message': f'Registration failed. Error: {str(e)}'
+        }, status=500)
 
-#     try:
-#         auth_logout(request)
-#         return JsonResponse({
-#             "username": username,
-#             "status": True,
-#             "message": "Logout berhasil!"
-#         }, status=200)
-#     except:
-#         return JsonResponse({
-#         "status": False,
-#         "message": "Logout gagal."
-#         }, status=401)
-
-# @csrf_exempt
-# def register(request):
-#     if request.method == 'POST':
-#         # Check if 'username', 'password', and 'email' keys are present in request.POST
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         email = request.POST.get('email')
-
-#         if username is not None and password is not None and email is not None:
-#             # Check if the username is already taken
-#             if User.objects.filter(username=username).exists():
-#                 return JsonResponse({'status': 'username_taken'})
-            
-#             # Create a new user
-#             user = User.objects.create_user(username=username, password=password, email=email)
-            
-#             # Log the user in
-#             auth_login(request, user)
-            
-#             # Status registrasi sukses.
-#             return JsonResponse({'status': 'success'})
-#         else:
-#             return JsonResponse({'status': 'missing_fields'})
-#     else:
-#         # Handle cases where the request method is not POST
-#         return JsonResponse({'status': 'method_not_allowed'})
 
 @csrf_exempt
 def login(request):
