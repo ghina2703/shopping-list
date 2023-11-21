f453354bc716dec5f672b3d8c326d787729cff96
ghina2703 <ghina2703@gmail.com>
2023-11-21 16:01:47 +0700

    deploy ulang tutorial 8

--- authentication/views.py
+++ authentication/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm
 
 
 @csrf_exempt
